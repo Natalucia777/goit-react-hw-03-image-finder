1fa92f1fb78a0a5ebf21fbda9830d366c8ad35ef
Natalya Demidenko <natalucia777@ukr.net>
2023-07-06 15:05:22 +0300

    hw-react-3-2-5

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+const notify = () => toast("Please, enter your query!");
 class Searchbar extends Component {
   state = {
     query: '',
   };
+  handleChange = ({ target }) => {
+    this.setState( { query: target.value.toLowerCase() });
+}
+  handleSubmit = e => {
+    e.preventDefault();
+    if (this.state.query.trim() === '') {
+      toast.error(notify);
+      return;
+    }
+    this.props.onSubmit(this.state.query);
+  }
 
   render() {
     <header class="searchbar">
-      <form class="form">
+      <form class="form" onSubmit={this.handleSubmit}>
         <button type="submit" class="button">
           <span class="button-label">Search</span>
         </button>
@@ -19,11 +33,16 @@ class Searchbar extends Component {
           autocomplete="off"
           autofocus
           placeholder="Search images and photos"
+          onImput={this.handleChange}
           value = {this.state.quary}
         />
       </form>
     </header>;
   }
 }
+
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
 
 export default Searchbar;
